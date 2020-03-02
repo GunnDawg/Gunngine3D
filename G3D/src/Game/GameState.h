c9f5e2568f31a86d09ee1a7748bfd14b02cd17b8
GunnDawg <cgunn86@gmail.com>
2020-03-02 00:30:29 -0800

    Fixed 'pure' static function initialization. Apprently 0u resolves to some other type than 'null' when passed here

--- G3D/src/Game/GameState.h
+++ G3D/src/Game/GameState.h
@@ -2,8 +2,8 @@
 
 struct GameState
 {
-	virtual void On_enter() = 0u;
-	virtual void On_exit() = 0u;
-	virtual void Handle_input() = 0u;
-	virtual void UpdateAndRender() = 0u;
+	virtual void On_enter() = 0;
+	virtual void On_exit() = 0;
+	virtual void Handle_input() = 0;
+	virtual void UpdateAndRender() = 0;
 };

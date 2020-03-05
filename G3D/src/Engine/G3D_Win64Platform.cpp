0e7d21c3fc7dc16c8c9e3c6517be0def9b331626
GunnDawg <cgunn86@gmail.com>
2020-03-04 23:56:36 -0800

    Minor change to a return value symantic

--- G3D/src/Engine/G3D_Win64Platform.cpp
+++ G3D/src/Engine/G3D_Win64Platform.cpp
@@ -81,7 +81,7 @@ WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 				}
 			}
 
-			return(DefWindowProc(hwnd, msg, wParam, lParam));
+			return DefWindowProc(hwnd, msg, wParam, lParam);
 		} break;
 
 		case WM_LBUTTONDOWN:

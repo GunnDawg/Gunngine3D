a6f424fbc2e7ff454d1b4c48f925c9390410b61f
GunnDawg <cgunn86@gmail.com>
2020-03-09 19:52:36 -0700

    Minor change to spacing in the vertices array

--- G3D/src/Engine/G3D_Mesh.cpp
+++ G3D/src/Engine/G3D_Mesh.cpp
@@ -7,8 +7,8 @@ void Mesh::Draw()
 
 	const BasicVertex vertices[] =
 	{
-		{DirectX::XMFLOAT3(0.0f, 0.5f, 1.0f)},
-		{DirectX::XMFLOAT3(0.5f, -0.5f, 1.0f)},
+		{DirectX::XMFLOAT3( 0.0f,  0.5f, 1.0f)},
+		{DirectX::XMFLOAT3( 0.5f, -0.5f, 1.0f)},
 		{DirectX::XMFLOAT3(-0.5f, -0.5f, 1.0f)}
 	};
 

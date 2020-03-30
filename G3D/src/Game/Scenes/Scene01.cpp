a116c84156338342c25b79649f97020192a97da1
GunnDawg <cgunn86@gmail.com>
2020-03-30 14:15:28 -0700

    Changed Camera FOV to 90

--- G3D/src/Game/Scenes/Scene01.cpp
+++ G3D/src/Game/Scenes/Scene01.cpp
@@ -20,8 +20,8 @@ bool Scene01::On_load()
 void Scene01::On_enter()
 {
 	OutputDebugString("S1 On_Enter\n");
-	Game::GameCamera.SetPosition(0.0f, 0.0f, 0.5f);
-	Game::GameCamera.SetProjectionValues(135.0f, (static_cast<float>(Settings::Display::Width) / static_cast<float>(Settings::Display::Height)), 0.1f, 10.0f);
+	Game::GameCamera.SetPosition(0.0f, 0.0f, 0.0f);
+	Game::GameCamera.SetProjectionValues(90.0f, (static_cast<float>(Settings::Display::Width) / static_cast<float>(Settings::Display::Height)), 0.1f, 10.0f);
 }
 
 void Scene01::On_exit()

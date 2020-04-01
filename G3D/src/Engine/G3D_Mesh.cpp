117fbac8852d96fc93154e5fd9e4e23214dbf9e4
GunnDawg <cgunn86@gmail.com>
2020-04-01 00:22:17 -0700

    Added more vertices for 3D, but it's broken

--- G3D/src/Engine/G3D_Mesh.cpp
+++ G3D/src/Engine/G3D_Mesh.cpp
@@ -15,6 +15,11 @@ namespace G3D
 
 		const TexturedVertex vertices[] =
 		{
+			TexturedVertex(-0.5f, -0.5f, -1.0f, 0.0f, 1.0f),
+			TexturedVertex(-0.5f,  0.5f, -1.0f, 0.0f, 0.0f),
+			TexturedVertex( 0.5f,  0.5f, -1.0f, 1.0f, 0.0f),
+			TexturedVertex( 0.5f, -0.5f, -1.0f, 1.0f, 1.0f),
+
 			TexturedVertex(-0.5f, -0.5f, 1.0f, 0.0f, 1.0f),
 			TexturedVertex(-0.5f,  0.5f, 1.0f, 0.0f, 0.0f),
 			TexturedVertex( 0.5f,  0.5f, 1.0f, 1.0f, 0.0f),
@@ -23,8 +28,12 @@ namespace G3D
 
 		const u16 indices[] =
 		{
-			0, 1, 2,
-			2, 3, 0
+			0,2,1, 2,3,1,
+			1,3,5, 3,7,5,
+			2,6,3, 3,6,7,
+			4,5,7, 4,7,6,
+			0,4,2, 2,4,6,
+			0,1,4, 1,5,4
 		};
 
 		IndexCount = (UINT)std::size(indices);

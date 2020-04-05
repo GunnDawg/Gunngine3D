aa9d76ac4807dc9ec090cc2790248c3a6581b829
GunnDawg <cgunn86@gmail.com>
2020-04-05 03:20:22 -0700

    Fixed Default texture shader

--- G3D/res/shaders/DefaultVertexShader.hlsl
+++ G3D/res/shaders/DefaultVertexShader.hlsl
@@ -10,10 +10,19 @@ struct VS_OUTPUT
     float2 outTexCoord : TEXCOORD;
 };
 
+cbuffer CameraBuffer : register(b0)
+{
+    matrix worldMatrix;
+    matrix viewMatrix;
+    matrix projMatrix;
+};
+
 VS_OUTPUT main(VS_INPUT input)
 {
     VS_OUTPUT output;
-    output.outPosition = input.inPosition;
+    matrix camera = transpose(mul(projMatrix, mul(viewMatrix, worldMatrix)));
+    
+    output.outPosition = mul(input.inPosition, camera);
     output.outTexCoord = input.inTexCoord;
     
     return output;

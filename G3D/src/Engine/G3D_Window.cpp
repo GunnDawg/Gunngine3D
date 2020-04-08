e03c5b257fe5e18e2cf5a71dfc5c71358c96fe0f
GunnDawg <cgunn86@gmail.com>
2020-04-07 17:52:31 -0700

    Fixed indentation

--- G3D/src/Engine/G3D_Window.cpp
+++ G3D/src/Engine/G3D_Window.cpp
@@ -4,15 +4,15 @@ namespace G3D
 {
 	bool Window::Initialize(const HINSTANCE& instance)
 	{
-		#if _DEBUG
-			Settings::Display::Windowed = true;
-			Settings::Display::Width = 1600u;
-			Settings::Display::Height = 900u;
-		#else
-			Settings::Display::Windowed = false;
-			Settings::Display::Width = GetSystemMetrics(SM_CXSCREEN);
-			Settings::Display::Height = GetSystemMetrics(SM_CYSCREEN);
-		#endif
+	#if _DEBUG
+		Settings::Display::Windowed = true;
+		Settings::Display::Width = 1600u;
+		Settings::Display::Height = 900u;
+	#else
+		Settings::Display::Windowed = false;
+		Settings::Display::Width = GetSystemMetrics(SM_CXSCREEN);
+		Settings::Display::Height = GetSystemMetrics(SM_CYSCREEN);
+	#endif
 
 		RECT wr;
 		wr.left = 0u;
@@ -32,18 +32,18 @@ namespace G3D
 		if (!RegisterClassEx(&wc))
 			return G3D_ERROR;
 
-#ifdef _DEBUG
+	#if _DEBUG
 		local_persist u16 centerX = (GetSystemMetrics(SM_CXSCREEN) - wr.right) / 2;
 		local_persist u16 centerY = (GetSystemMetrics(SM_CYSCREEN) - wr.bottom) / 2;
 
 		WindowHandle = CreateWindowEx(0, wc.lpszClassName, "Gunngine3D", WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX | WS_VISIBLE, centerX, centerY, wr.right - wr.left, wr.bottom - wr.top, 0, 0, instance, 0);
 		if (!WindowHandle)
 			return G3D_ERROR;
-#else
+	#else
 		WindowHandle = CreateWindowEx(0, wc.lpszClassName, "Gunngine3D", WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX | WS_VISIBLE, wr.left, wr.top, wr.right - wr.left, wr.bottom - wr.top, 0, 0, instance, 0);
 		if (!WindowHandle)
 			return G3D_ERROR;
-#endif
+	#endif
 
 		return G3D_OK;
 	}

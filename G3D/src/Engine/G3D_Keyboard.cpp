ec7970cf26a1d83ef86ccb8799dd797d13a2fd13
GunnDawg <cgunn86@gmail.com>
2020-04-10 22:09:20 -0700

    Overhaul of keyboard system

--- G3D/src/Engine/G3D_Keyboard.cpp
+++ G3D/src/Engine/G3D_Keyboard.cpp
@@ -1,29 +1,104 @@
 #include "G3D_Keyboard.h"
 
-namespace G3D
+bool G3D::Keyboard::KeyIsPressed(unsigned char keycode) const noexcept
 {
-	Keyboard::Event Keyboard::ReadKey()
+	return keystates[keycode];
+}
+
+std::optional<G3D::Keyboard::Event> G3D::Keyboard::ReadKey() noexcept
+{
+	if (keybuffer.size() > 0u)
 	{
-		if (keyBuffer.size() > 0u)
-		{
-			Keyboard::Event e = keyBuffer.front();
-			keyBuffer.pop();
-			return e ;
-		}
-		else
-			return Keyboard::Event();
+		Keyboard::Event e = keybuffer.front();
+		keybuffer.pop();
+		return e;
 	}
+	return {};
+}
 
-	u16 Keyboard::ReadChar()
+bool G3D::Keyboard::KeyIsEmpty() const noexcept
+{
+	return keybuffer.empty();
+}
+
+std::optional<char> G3D::Keyboard::ReadChar() noexcept
+{
+	if (charbuffer.size() > 0u)
 	{
-		if (charBuffer.size() > 0u)
-		{
-			u16 charcode = charBuffer.front();
-			charBuffer.pop();
+		unsigned char charcode = charbuffer.front();
+		charbuffer.pop();
+		return charcode;
+	}
+	return {};
+}
 
-			return charcode;
-		}
-		else
-			return 0;
+bool G3D::Keyboard::CharIsEmpty() const noexcept
+{
+	return charbuffer.empty();
+}
+
+void G3D::Keyboard::FlushKey() noexcept
+{
+	keybuffer = std::queue<Event>();
+}
+
+void G3D::Keyboard::FlushChar() noexcept
+{
+	charbuffer = std::queue<char>();
+}
+
+void G3D::Keyboard::Flush() noexcept
+{
+	FlushKey();
+	FlushChar();
+}
+
+void G3D::Keyboard::EnableAutorepeat() noexcept
+{
+	autorepeatEnabled = true;
+}
+
+void G3D::Keyboard::DisableAutorepeat() noexcept
+{
+	autorepeatEnabled = false;
+}
+
+bool G3D::Keyboard::AutorepeatIsEnabled() const noexcept
+{
+	return autorepeatEnabled;
+}
+
+void G3D::Keyboard::OnKeyPressed(unsigned char keycode) noexcept
+{
+	keystates[keycode] = true;
+	keybuffer.push(Keyboard::Event(Keyboard::Event::Type::Press, keycode));
+	TrimBuffer(keybuffer);
+}
+
+void G3D::Keyboard::OnKeyReleased(unsigned char keycode) noexcept
+{
+	keystates[keycode] = false;
+	keybuffer.push(Keyboard::Event(Keyboard::Event::Type::Release, keycode));
+	TrimBuffer(keybuffer);
+}
+
+void G3D::Keyboard::OnChar(char character) noexcept
+{
+	charbuffer.push(character);
+	TrimBuffer(charbuffer);
+}
+
+void G3D::Keyboard::ClearState() noexcept
+{
+	keystates.reset();
+}
+
+template<typename T>
+void G3D::Keyboard::TrimBuffer(std::queue<T>& buffer) noexcept
+{
+	while (buffer.size() > bufferSize)
+	{
+		buffer.pop();
 	}
 }
+
--- G3D/src/Engine/G3D_Keyboard.h
+++ G3D/src/Engine/G3D_Keyboard.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <queue>
 #include <bitset>
+#include <optional>
 
 namespace G3D
 {
 	class Keyboard
 	{
+		friend class Window;
 	public:
 		class Event
 		{
@@ -14,93 +16,60 @@ namespace G3D
 			{
 				Press,
 				Release,
-				Invalid
 			};
-
 		private:
 			Type type;
-			u16 code;
-
+			unsigned char code;
 		public:
-			Event() :
-				type(Type::Invalid),
-				code(0u)
-			{}
-
-			Event(Type type, u16 code) :
+			Event(Type type, unsigned char code) noexcept
+				:
 				type(type),
 				code(code)
 			{}
-
-			inline bool IsPress() const { return type == Type::Press; }
-			inline bool IsRelease() const { return type == Type::Release; }
-			inline bool IsValid() const { return type != Type::Invalid; }
-			inline u16 GetCode() const { return code; }
+			bool IsPress() const noexcept
+			{
+				return type == Type::Press;
+			}
+			bool IsRelease() const noexcept
+			{
+				return type == Type::Release;
+			}
+			unsigned char GetCode() const noexcept
+			{
+				return code;
+			}
 		};
-
 	public:
 		Keyboard() = default;
-		Keyboard(const Keyboard& kb) = delete;
-		Keyboard& operator= (const Keyboard& kb) = delete;
-
-		//Key event stuff
-		Event ReadKey();
-		inline bool KeyIsPressed(u16 keycode) const { return keyStates[keycode]; }
-		inline bool KeyIsEmpty() const { return keyBuffer.empty(); }
-		inline void FlushKey() { keyBuffer = std::queue<Event>(); }
-
-		//Char event stuff
-		u16 ReadChar();
-		inline bool CharIsEmpty() const { return charBuffer.empty(); }
-		inline void FlushChar() { charBuffer = std::queue<u16>(); }
-		inline void Flush()
-		{
-			FlushKey();
-			FlushChar();
-		};
-
-		//Autorepeat control
-		inline void EnableAutorepeat() { autorepeatEnabled = true; }
-		inline void DisableAutorepeat() { autorepeatEnabled = false; }
-		inline bool AutorepeatIsEnabled() const { return autorepeatEnabled; }
-
-		inline void OnKeyPressed(u16 keycode)
-		{
-			keyStates[keycode] = true;
-			keyBuffer.push(Keyboard::Event(Keyboard::Event::Type::Press, keycode));
-			TrimBuffer(keyBuffer);
-		}
-
-		inline void OnKeyReleased(u16 keycode)
-		{
-			keyStates[keycode] = false;
-			keyBuffer.push(Keyboard::Event(Keyboard::Event::Type::Release, keycode));
-			TrimBuffer(keyBuffer);
-		}
-
-		inline void OnChar(u16 character)
-		{
-			charBuffer.push(character);
-			TrimBuffer(charBuffer);
-		}
-
-		inline void ClearState() { keyStates.reset(); }
-
-		template<typename T>
-		inline static void TrimBuffer(std::queue<T>& buffer)
-		{
-			while (buffer.size() > bufferSize)
-			{
-				buffer.pop();
-			}
-		}
-
+		Keyboard(const Keyboard&) = delete;
+		Keyboard& operator=(const Keyboard&) = delete;
+		// key event stuff
+		bool KeyIsPressed(unsigned char keycode) const noexcept;
+		std::optional<Event> ReadKey() noexcept;
+		bool KeyIsEmpty() const noexcept;
+		void FlushKey() noexcept;
+		// char event stuff
+		std::optional<char> ReadChar() noexcept;
+		bool CharIsEmpty() const noexcept;
+		void FlushChar() noexcept;
+		void Flush() noexcept;
+		// autorepeat control
+		void EnableAutorepeat() noexcept;
+		void DisableAutorepeat() noexcept;
+		bool AutorepeatIsEnabled() const noexcept;
+		void OnKeyPressed(unsigned char keycode) noexcept;
+		void OnKeyReleased(unsigned char keycode) noexcept;
+		void OnChar(char character) noexcept;
 	private:
-		static constexpr u16 nKeys = 256u;
-		static constexpr u16 bufferSize = 16u;
+		void ClearState() noexcept;
+		template<typename T>
+		static void TrimBuffer(std::queue<T>& buffer) noexcept;
+	private:
+		static constexpr unsigned int nKeys = 256u;
+		static constexpr unsigned int bufferSize = 16u;
 		bool autorepeatEnabled = false;
-		std::bitset<nKeys> keyStates;
-		std::queue<Event> keyBuffer;
-		std::queue<u16> charBuffer;
+		std::bitset<nKeys> keystates;
+		std::queue<Event> keybuffer;
+		std::queue<char> charbuffer;
 	};
 }

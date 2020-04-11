d8e29a461b4238584192b0bce5570abfaea163c0
GunnDawg <cgunn86@gmail.com>
2020-04-11 00:26:32 -0700

    Minor changes to debug information layout. Version is now it's own variable

--- G3D/src/Engine/G3D_Font.cpp
+++ G3D/src/Engine/G3D_Font.cpp
@@ -33,16 +33,17 @@ namespace G3D
 		//@MOVE: All of this needs to be moved out into some debug structure so that this class
 		//is not specific to printing ONLY debug information.
 		local_persist char Buffer[512];
+		local_persist const char* Build;
+		local_persist const char* Version = "0.0.0.1";
+		local_persist const char* Status;
 		local_persist const char* VSync;
 		local_persist const char* MSAA;
-		local_persist const char* Build;
 		local_persist const char* Camera;
 		local_persist float DrawDistance = Settings::Graphics::DrawDistance;
 		local_persist float FOV = Settings::Camera::FOV;
 
 
 		//@PERFORMANCE: How much of a performance cost are we incurring by declaring these every frame?
-		local_persist const char* Status = "test";
 		float DeltaTime = G3D::Core::PerformanceClock.DeltaTime;
 		float FPS = G3D::Core::PerformanceClock.FPS;
 		float MCPF = G3D::Core::PerformanceClock.MCPF;
@@ -75,7 +76,8 @@ namespace G3D
 		#else
 			Build = "Release";
 		#endif
-		sprintf(Buffer, "Build: %s\nVersion: 0.0.0.1\n\nPerformance Information:\n  Status: %s\n  Frame Time: %.02fms\n  FPS: %.02f\n  Millicycles Per Frame: %.02f\n\nCamera Settings:\n  Mode: %s\n  FOV: %.02f\n\  Position:\n   X: %.02f\n   Y: %.02f\n   Z: %0.2f\n\nVideo Settings:\n  VSync: %s\n  MSAA: %s\n  Draw Distance: %.02f", Build, Status, DeltaTime, FPS, MCPF, Camera, FOV, CameraX, CameraY, CameraZ, VSync, MSAA, DrawDistance);
+
+		sprintf(Buffer, "Build: %s\nVersion: %s\n\nPerformance Information:\n  Status: %s\n  Frame Time: %.02fms\n  FPS: %.02f\n  Millicycles Per Frame: %.02f\n\nCamera Settings:\n  Mode: %s\n  FOV: %.02f\n  Position:\n   X: %.02f\n   Y: %.02f\n   Z: %0.2f\n\nVideo Settings:\n  VSync: %s\n  MSAA: %s\n  Draw Distance: %.02f", Build, Version, Status, DeltaTime, FPS, MCPF, Camera, FOV, CameraX, CameraY, CameraZ, VSync, MSAA, DrawDistance);
 		//sprintf(Buffer, "Build: %s\nVersion: 0.0.0.1\n\nPerformance Information:\n  Frame Time: %.02fms\n  FPS: %.02f\n  Millicycles Per Frame: %.02f\n\nCamera Settings:\n  Mode: %s\n  FOV: %.02f\n\  Position:\n   X: %.02f\n   Y: %.02f\n   Z: %0.2f\n\nVideo Settings:\n  VSync: %s\n  MSAA: %s\n  Draw Distance: %.02f", Build, G3D::Core::PerformanceClock.DeltaTime, G3D::Core::PerformanceClock.FPS, G3D::Core::PerformanceClock.MCPF, Camera, Settings::Camera::FOV, Game::GameCamera.GetCameraPosX(), Game::GameCamera.GetCameraPosY(), Game::GameCamera.GetCameraPosZ(), VSync, MSAA, Settings::Graphics::DrawDistance);
 
 		spriteBatch->Begin();

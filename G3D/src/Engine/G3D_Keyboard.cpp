9df4085b39bad99bc0542a93edea5cebde8fd2d5
GunnDawg <cgunn86@gmail.com>
2020-04-10 22:41:07 -0700

    Added Keyboard.cpp to the G3D namespace scope

--- G3D/src/Engine/G3D_Keyboard.cpp
+++ G3D/src/Engine/G3D_Keyboard.cpp
@@ -1,104 +1,107 @@
 #include "G3D_Keyboard.h"
 
-bool G3D::Keyboard::KeyIsPressed(unsigned char keycode) const noexcept
+namespace G3D
 {
-	return keystates[keycode];
-}
-
-std::optional<G3D::Keyboard::Event> G3D::Keyboard::ReadKey() noexcept
-{
-	if (keybuffer.size() > 0u)
+	bool Keyboard::KeyIsPressed(unsigned char keycode) const noexcept
 	{
-		Keyboard::Event e = keybuffer.front();
-		keybuffer.pop();
-		return e;
+		return keystates[keycode];
 	}
-	return {};
-}
 
-bool G3D::Keyboard::KeyIsEmpty() const noexcept
-{
-	return keybuffer.empty();
-}
-
-std::optional<char> G3D::Keyboard::ReadChar() noexcept
-{
-	if (charbuffer.size() > 0u)
+	std::optional<Keyboard::Event> Keyboard::ReadKey() noexcept
 	{
-		unsigned char charcode = charbuffer.front();
-		charbuffer.pop();
-		return charcode;
+		if (keybuffer.size() > 0u)
+		{
+			Keyboard::Event e = keybuffer.front();
+			keybuffer.pop();
+			return e;
+		}
+		return {};
 	}
-	return {};
-}
 
-bool G3D::Keyboard::CharIsEmpty() const noexcept
-{
-	return charbuffer.empty();
-}
-
-void G3D::Keyboard::FlushKey() noexcept
-{
-	keybuffer = std::queue<Event>();
-}
-
-void G3D::Keyboard::FlushChar() noexcept
-{
-	charbuffer = std::queue<char>();
-}
-
-void G3D::Keyboard::Flush() noexcept
-{
-	FlushKey();
-	FlushChar();
-}
-
-void G3D::Keyboard::EnableAutorepeat() noexcept
-{
-	autorepeatEnabled = true;
-}
-
-void G3D::Keyboard::DisableAutorepeat() noexcept
-{
-	autorepeatEnabled = false;
-}
-
-bool G3D::Keyboard::AutorepeatIsEnabled() const noexcept
-{
-	return autorepeatEnabled;
-}
-
-void G3D::Keyboard::OnKeyPressed(unsigned char keycode) noexcept
-{
-	keystates[keycode] = true;
-	keybuffer.push(Keyboard::Event(Keyboard::Event::Type::Press, keycode));
-	TrimBuffer(keybuffer);
-}
-
-void G3D::Keyboard::OnKeyReleased(unsigned char keycode) noexcept
-{
-	keystates[keycode] = false;
-	keybuffer.push(Keyboard::Event(Keyboard::Event::Type::Release, keycode));
-	TrimBuffer(keybuffer);
-}
-
-void G3D::Keyboard::OnChar(char character) noexcept
-{
-	charbuffer.push(character);
-	TrimBuffer(charbuffer);
-}
-
-void G3D::Keyboard::ClearState() noexcept
-{
-	keystates.reset();
-}
-
-template<typename T>
-void G3D::Keyboard::TrimBuffer(std::queue<T>& buffer) noexcept
-{
-	while (buffer.size() > bufferSize)
+	bool Keyboard::KeyIsEmpty() const noexcept
 	{
-		buffer.pop();
+		return keybuffer.empty();
+	}
+
+	std::optional<char> Keyboard::ReadChar() noexcept
+	{
+		if (charbuffer.size() > 0u)
+		{
+			unsigned char charcode = charbuffer.front();
+			charbuffer.pop();
+			return charcode;
+		}
+		return {};
+	}
+
+	bool Keyboard::CharIsEmpty() const noexcept
+	{
+		return charbuffer.empty();
+	}
+
+	void Keyboard::FlushKey() noexcept
+	{
+		keybuffer = std::queue<Event>();
+	}
+
+	void Keyboard::FlushChar() noexcept
+	{
+		charbuffer = std::queue<char>();
+	}
+
+	void Keyboard::Flush() noexcept
+	{
+		FlushKey();
+		FlushChar();
+	}
+
+	void Keyboard::EnableAutorepeat() noexcept
+	{
+		autorepeatEnabled = true;
+	}
+
+	void Keyboard::DisableAutorepeat() noexcept
+	{
+		autorepeatEnabled = false;
+	}
+
+	bool Keyboard::AutorepeatIsEnabled() const noexcept
+	{
+		return autorepeatEnabled;
+	}
+
+	void Keyboard::OnKeyPressed(unsigned char keycode) noexcept
+	{
+		keystates[keycode] = true;
+		keybuffer.push(Keyboard::Event(Keyboard::Event::Type::Press, keycode));
+		TrimBuffer(keybuffer);
+	}
+
+	void Keyboard::OnKeyReleased(unsigned char keycode) noexcept
+	{
+		keystates[keycode] = false;
+		keybuffer.push(Keyboard::Event(Keyboard::Event::Type::Release, keycode));
+		TrimBuffer(keybuffer);
+	}
+
+	void Keyboard::OnChar(char character) noexcept
+	{
+		charbuffer.push(character);
+		TrimBuffer(charbuffer);
+	}
+
+	void Keyboard::ClearState() noexcept
+	{
+		keystates.reset();
+	}
+
+	template<typename T>
+	void Keyboard::TrimBuffer(std::queue<T>& buffer) noexcept
+	{
+		while (buffer.size() > bufferSize)
+		{
+			buffer.pop();
+		}
 	}
 }
 

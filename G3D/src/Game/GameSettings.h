3bb59607541e5490097a88047b2a9dd6c0bc4941
GunnDawg <cgunn86@gmail.com>
2020-04-24 05:22:06 -0700

    Minor changes to the way we organize the namespace tree

--- G3D/src/Game/GameSettings.h
+++ G3D/src/Game/GameSettings.h
@@ -1,33 +1,36 @@
 #pragma once
 
-namespace Settings::Display
+namespace Settings
 {
-	static u16 Width = 0u;
-	static u16 Height = 0u;
-	static bool Windowed = true;
-	static bool VSync = true;
-}
+	namespace Display
+	{
+		static u16 Width = 0u;
+		static u16 Height = 0u;
+		static bool Windowed = true;
+		static bool VSync = true;
+	}
 
-namespace Settings::Audio
-{
+	namespace Audio
+	{
 
-}
+	}
 
-namespace Settings::Graphics
-{
-	static bool MSAA = true;
-	static UINT MSAAQuality = 0u;
-	static float DrawDistance = 100.0f;
-}
+	namespace Graphics
+	{
+		static bool MSAA = true;
+		static UINT MSAAQuality = 0u;
+		static float DrawDistance = 100.0f;
+	}
 
-namespace Settings::Controls
-{
-	static float MouseSensitivity = 0.005f;
-}
+	namespace Controls
+	{
+		static float MouseSensitivity = 0.005f;
+	}
 
-namespace Settings::Camera
-{
-	static bool FreeRoam = true;
-	static float FOV = 59.0f; // Vertical FoV
-	static float CameraSpeed = 0.02f;
+	namespace Camera
+	{
+		static bool FreeRoam = true;
+		static float FOV = 59.0f; // Vertical FoV
+		static float CameraSpeed = 0.02f;
+	}
 }

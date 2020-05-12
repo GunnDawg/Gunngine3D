9395cf704b0a6bcd6c4870e4dacb4cd36e9dfcd4
GunnDawg <cgunn86@gmail.com>
2020-05-12 04:07:12 -0700

    Minor changes to comment formatting

--- G3D/src/Engine/G3D_Mesh.cpp
+++ G3D/src/Engine/G3D_Mesh.cpp
@@ -15,36 +15,37 @@ namespace G3D
 
 		const TexturedVertex vertices[] =
 		{
+			//Front Face
 			TexturedVertex(-1.0f, -1.0f, -1.0f, 0.0f, 1.0f,-1.0f, -1.0f, -1.0f),
 			TexturedVertex(-1.0f,  1.0f, -1.0f, 0.0f, 0.0f,-1.0f,  1.0f, -1.0f),
 			TexturedVertex( 1.0f,  1.0f, -1.0f, 1.0f, 0.0f, 1.0f,  1.0f, -1.0f),
 			TexturedVertex( 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f, -1.0f),
 
-			// Back Face
+			//Back Face
 			TexturedVertex(-1.0f, -1.0f, 1.0f, 1.0f, 1.0f,-1.0f, -1.0f, 1.0f),
 			TexturedVertex( 1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 1.0f, -1.0f, 1.0f),
 			TexturedVertex( 1.0f,  1.0f, 1.0f, 0.0f, 0.0f, 1.0f,  1.0f, 1.0f),
 			TexturedVertex(-1.0f,  1.0f, 1.0f, 1.0f, 0.0f,-1.0f,  1.0f, 1.0f),
 
-			// Top Face
+			//Top Face
 			TexturedVertex(-1.0f, 1.0f, -1.0f, 0.0f, 1.0f,-1.0f, 1.0f, -1.0f),
 			TexturedVertex(-1.0f, 1.0f,  1.0f, 0.0f, 0.0f,-1.0f, 1.0f,  1.0f),
 			TexturedVertex( 1.0f, 1.0f,  1.0f, 1.0f, 0.0f, 1.0f, 1.0f,  1.0f),
 			TexturedVertex( 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f),
 
-			// Bottom Face
+			//Bottom Face
 			TexturedVertex(-1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, -1.0f),
 			TexturedVertex( 1.0f, -1.0f, -1.0f, 0.0f, 1.0f,  1.0f, -1.0f, -1.0f),
 			TexturedVertex( 1.0f, -1.0f,  1.0f, 0.0f, 0.0f,  1.0f, -1.0f,  1.0f),
 			TexturedVertex(-1.0f, -1.0f,  1.0f, 1.0f, 0.0f, -1.0f, -1.0f,  1.0f),
 
-			// Left Face
+			//Left Face
 			TexturedVertex(-1.0f, -1.0f,  1.0f, 0.0f, 1.0f, -1.0f, -1.0f,  1.0f),
 			TexturedVertex(-1.0f,  1.0f,  1.0f, 0.0f, 0.0f, -1.0f,  1.0f,  1.0f),
 			TexturedVertex(-1.0f,  1.0f, -1.0f, 1.0f, 0.0f, -1.0f,  1.0f, -1.0f),
 			TexturedVertex(-1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, -1.0f),
 
-			// Right Face
+			//Right Face
 			TexturedVertex(1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 1.0f, -1.0f, -1.0f),
 			TexturedVertex(1.0f,  1.0f, -1.0f, 0.0f, 0.0f, 1.0f,  1.0f, -1.0f),
 			TexturedVertex(1.0f,  1.0f,  1.0f, 1.0f, 0.0f, 1.0f,  1.0f,  1.0f),
@@ -53,27 +54,27 @@ namespace G3D
 
 		const u16 indices[] =
 		{
-			// Front Face
+			//Front Face
 			0, 1, 2,
 			0, 2, 3,
 
-			// Back Face
+			//Back Face
 			4, 5, 6,
 			4, 6, 7,
 
-			// Top Face
+			//Top Face
 			8, 9, 10,
 			8, 10, 11,
 
-			// Bottom Face
+			//Bottom Face
 			12, 13, 14,
 			12, 14, 15,
 
-			// Left Face
+			//Left Face
 			16, 17, 18,
 			16, 18, 19,
 
-			// Right Face
+			//Right Face
 			20, 21, 22,
 			20, 22, 23
 		};

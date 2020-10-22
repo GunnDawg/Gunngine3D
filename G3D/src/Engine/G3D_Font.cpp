6e43d31e270b3a136023f8e8f32e8e485f1a6170
GunnDawg <cgunn86@gmail.com>
2020-10-21 21:14:24 -0700

    Added shader version: debug/release to the list of debug panel information

--- G3D/src/Engine/G3D_Font.cpp
+++ G3D/src/Engine/G3D_Font.cpp
@@ -57,6 +57,7 @@ namespace G3D
 				local_persist const float CSpeed = Settings::Camera::CameraSpeed * 100.0f;
 				local_persist const float cSens = Settings::Controls::MouseSensitivity * 1000.0f;
 				local_persist float DrawDistance = Settings::Graphics::DrawDistance;
+				local_persist const char* ShaderVersion;
 				local_persist float FOV = Settings::Camera::FOV;
 
 				//@NOTE: These get updated every frame
@@ -104,12 +105,18 @@ namespace G3D
 					MSAA = "Off";
 
 				#if _DEBUG
+					ShaderVersion = "Debug";
+				#else
+					ShaderVersion = "Release";
+				#endif
+
+				#if _DEBUG
 					Build = "Debug";
 				#else
 					Build = "Release";
 				#endif
 
-				sprintf(Buffer, "'F1' Hide System Information\n'F2' Show Lighting Information\n\n'Esc' Back\n'~' Disable Overlay\n'Q' Quit\n\nBuild: %s\nVersion: %s\n\nPerformance Information:\n  Status: %s\n  Runtime Clock: %is\n  Frame Time: %.02fms\n  FPS: %.02f\n  CPU MCPF: %.02f\n\nVideo Settings:\n  Resolution: %ix%i\n  VSync: %s\n  MSAA: %s\n  Draw Distance: %.02f\n\nInput Settings:\n  Device: %s\n  Look Sens: %.02f\n\nCamera Settings:\n  Mode: %s\n  vFOV: %.02f\n  Speed: %.02f\n  Position:\n   X: %.02f\n   Y: %.02f\n   Z: %0.2f", Build, Version, Status, Clock, DeltaTime, FPS, MCPF, Width, Height, VSync, MSAA, DrawDistance, InputType, cSens, Camera, FOV, CSpeed, CameraX, CameraY, CameraZ);
+				sprintf(Buffer, "'F1' Hide System Information\n'F2' Show Lighting Information\n\n'Esc' Back\n'~' Disable Overlay\n'Q' Quit\n\nBuild: %s\nVersion: %s\n\nPerformance Information:\n  Status: %s\n  Runtime Clock: %is\n  Frame Time: %.02fms\n  FPS: %.02f\n  CPU MCPF: %.02f\n\nVideo Settings:\n  Resolution: %ix%i\n  VSync: %s\n  MSAA: %s\n  Draw Distance: %.02f\n  Shader Version: %s\n\nInput Settings:\n  Device: %s\n  Look Sens: %.02f\n\nCamera Settings:\n  Mode: %s\n  vFOV: %.02f\n  Speed: %.02f\n  Position:\n   X: %.02f\n   Y: %.02f\n   Z: %0.2f", Build, Version, Status, Clock, DeltaTime, FPS, MCPF, Width, Height, VSync, MSAA, DrawDistance, ShaderVersion, InputType, cSens, Camera, FOV, CSpeed, CameraX, CameraY, CameraZ);
 
 				spriteBatch->Begin();
 				spriteFont->DrawString(spriteBatch.get(), Buffer, DirectX::XMFLOAT2(0.0f, 0.0f), DirectX::Colors::White, 0.0f, DirectX::XMFLOAT2(0.0f, 0.0f), DirectX::XMFLOAT2(1.0f, 1.0f));

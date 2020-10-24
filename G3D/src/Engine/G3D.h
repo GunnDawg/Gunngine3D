82c51f2e900250f66b7a5af9737065a04b471884
GunnDawg <cgunn86@gmail.com>
2020-10-24 02:38:36 -0700

    Removed some VS warnings with comments and debug .dcso files

--- G3D/src/Engine/G3D.h
+++ G3D/src/Engine/G3D.h
@@ -2,10 +2,8 @@
 #if 1
 	//Conversion Warnings that we'll fix at some point
 	#pragma warning(disable:4244)
-
-	//May use in the future if they come back. Forgot right now what they were exactly.
-	//#pragma warning(disable:6011)
-	//#pragma warning(disable:26812)
+	#pragma warning(disable:6011)
+	#pragma warning(disable:26812)
 #endif
 
 #include <stdint.h>
